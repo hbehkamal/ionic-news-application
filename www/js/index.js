9d9a7cbc8d43392454c0d46424987a926f5fc1eb
HoseinBL <hoseynbehkamal@gmail.com>
2015-12-27 00:01:57 +0330

    fix some cherts :D

--- www/js/index.js
+++ www/js/index.js
@@ -6,6 +6,7 @@
     'ionic',
     'myapp.config',
     'myapp.controllers',
+    'myapp.services',
     ])
   ;
 })(angular);
--- www/js/services.js
+++ www/js/services.js
@@ -33,8 +33,19 @@
           }
         ];
         return {
-          all: function(){
-            return news;
+          all: function() {
+            return chats;
+          },
+          remove: function(chat) {
+            chats.splice(chats.indexOf(chat), 1);
+          },
+          get: function(chatId) {
+            for (var i = 0; i < chats.length; i++) {
+              if (chats[i].id === parseInt(chatId)) {
+                return chats[i];
+              }
+            }
+            return null;
           }
         };
     });
